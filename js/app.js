687ad2f9ab9e2709f904ea1f3ebeae7da145ff1a
Nazrul <mohammadnazrulislam2k20@gmail.com>
2022-08-09 20:34:06 +0600

    Adding the verifying js code

--- js/app.js
+++ js/app.js
@@ -1,50 +1,3 @@
-/* function getPin(){
-    const pin = Math.round(Math.random()*10000);
-    const pinString = pin + '';
-    if(pinString.length == 4){
-        return pin;
-    }
-    else{
-        return getPin();
-    }
-}
-
-function generatePin(){
-    const pin = getPin();
-    document.getElementById('display-pin').value = pin;
-    
-}
-
-document.getElementById('key-pad').addEventListener('click', function(event){
-    const calcInput = document.getElementById('typed-numbers');
-    const number = (event.target.innerText);
-    if(isNaN(number)){
-        if(number == 'C'){
-            calcInput.value = '';
-        }
-    }
-    else{
-        const previousNumber = calcInput.value;
-        const newNumber = previousNumber + number;
-        calcInput.value = newNumber;
-    }
-})
-
-function verifyPin(){
-    const generatedPin = document.getElementById('display-pin').value;
-    const typedPin = document.getElementById('typed-numbers').value;
-    const successMessage = document.getElementById('success');
-    const failError = document.getElementById('fail');
-    if(generatedPin == typedPin){
-        failError.style.display = 'none';
-        successMessage.style.display = 'block';
-    }
-    else{
-        successMessage.style.display = 'none';
-        failError.style.display = 'block';
-    }
-} */
-
 function getPin(){
     const pin = Math.round(Math.random()*10000);
     const pinString = pin + '';
@@ -59,5 +12,32 @@ function getPin(){
 function generatePin(){
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
+}
 
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const typedInput = document.getElementById('typed-numbers');
+    if(isNaN(number)){
+        if(number == 'C'){
+            typedInput.value = '';
+        }
+    }
+    else{
+        const previousNumber = typedInput.value;
+        const newNumber = previousNumber + number;
+        typedInput.value = newNumber;
+    }
+})
+
+function verifyPin(){
+    const generatedPin = document.getElementById('display-pin').value;
+    const typedPin = document.getElementById('typed-numbers').value;
+    if(generatedPin == typedPin){
+        document.getElementById('success').style.display = 'block';
+        document.getElementById('fail').style.display = 'none';
+    }
+    else{
+        document.getElementById('fail').style.display = 'block';
+        document.getElementById('success').style.display = 'none';
+    }
 }
